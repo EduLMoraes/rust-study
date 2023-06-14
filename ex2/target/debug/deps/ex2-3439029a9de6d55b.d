9b03a719a7270d1a8eea8981d2c16d5c1be26059
EduardoMoreaes <17eduardo05@gmail.com>
2023-06-14 16:34:23 -0300

    #3

---
+++ ex2/target/debug/deps/ex2-3439029a9de6d55b.d
@@ -0,0 +1,5 @@
+/home/eduardo/Documentos/TADS/Testes/rust/ex2/target/debug/deps/ex2-3439029a9de6d55b.rmeta: src/main.rs
+
+/home/eduardo/Documentos/TADS/Testes/rust/ex2/target/debug/deps/ex2-3439029a9de6d55b.d: src/main.rs
+
+src/main.rs:
